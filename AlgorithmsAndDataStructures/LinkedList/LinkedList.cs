7fb225b13c1bc6df713dc285ff485cdcac2b9da6
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-07-04 23:40:01 +0300

    Add LinkedList prototype

---
+++ AlgorithmsAndDataStructures/LinkedList/LinkedList.cs
@@ -0,0 +1,40 @@
+﻿namespace LinkedList
+{
+    class LinkedList
+    {
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+        public int Count { get; private set; }
+
+        public void AddFirst(Node node)
+        {
+            var temp = Head;
+
+            Head = node;
+
+            Head.Next = temp;
+
+            Count++;
+
+            if (Count == 1)
+            {
+                Tail = Head;
+            }
+        }
+
+        public void AddLast(Node node)
+        {
+            if (Count == 0)
+            {
+                Head = node;
+            }
+            else
+            {
+                Tail.Next = node;
+            }
+
+            Tail = node;
+            Count++;
+        }
+    }
+}
