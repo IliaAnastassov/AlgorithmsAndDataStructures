6caf357c7f4eb0e0f8f251e400c27617df975974
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-07-05 12:20:44 +0300

    Review node structure

--- AlgorithmsAndDataStructures/LinkedList/LinkedListNode.cs
+++ AlgorithmsAndDataStructures/LinkedList/LinkedListNode.cs
@@ -1,103 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-
-namespace LinkedList
+﻿namespace LinkedList
 {
-    class LinkedListNode<T> : IEnumerable<T>
+    /// <summary>
+    /// A node in the linked list
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class LinkedListNode<T>
     {
+        /// <summary>
+        /// The node value
+        /// </summary>
         public T Value { get; set; }
 
+        /// <summary>
+        /// The next node in the linked list (null if last node)
+        /// </summary>
         public LinkedListNode<T> Next { get; set; }
 
-        public LinkedListNode<T> Head { get; set; }
-
-        public LinkedListNode<T> Tail { get; set; }
-
-        public int Count { get; private set; }
-
-        public void AddFirst(LinkedListNode<T> node)
+        /// <summary>
+        /// Constructs a new node with the specified value
+        /// </summary>
+        /// <param name="value"></param>
+        public LinkedListNode(T value)
         {
-            LinkedListNode<T> temp = Head;
-
-            Head = node;
-
-            Head.Next = temp;
-
-            Count++;
-
-            if (Count == 1)
-            {
-                Tail = Head;
-            }
-        }
-
-        public void AddLast(LinkedListNode<T> node)
-        {
-            if (Count == 0)
-            {
-                Head = node;
-            }
-            else
-            {
-                Tail.Next = node;
-            }
-
-            Tail = node;
-            Count++;
-        }
-
-        public void RemoveLast()
-        {
-            if (Count != 0)
-            {
-                if (Count == 1)
-                {
-                    Head = null;
-                    Tail = null;
-                }
-                else
-                {
-                    LinkedListNode<T> current = Head;
-                    while (current.Next != Tail)
-                    {
-                        current = current.Next;
-                    }
-
-                    current.Next = null;
-                    Tail = current;
-                }
-
-                Count--;
-            }
-        }
-
-        public void RemoveFirst()
-        {
-            if (Count != 0)
-            {
-                Head = Head.Next;
-                Count--;
-
-                if (Count == 0)
-                {
-                    Tail = null;
-                }
-            }
-        }
-
-        IEnumerator<T> IEnumerable<T>.GetEnumerator()
-        {
-            LinkedListNode<T> current = Head;
-            while (current != null)
-            {
-                yield return current.Value;
-                current = current.Next;
-            }
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new System.NotImplementedException();
+            Value = value;
         }
     }
 }
