d0e4ae0dd6143c1a13c0b7c009c9446261d1eb04
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-07-08 19:09:34 +0300

    Update Stack

--- AlgorithmsAndDataStructures/Stack.List/Stack.cs
+++ AlgorithmsAndDataStructures/Stack.List/Stack.cs
@@ -1,20 +1,99 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Stack.List
 {
     class Stack<T> : IEnumerable<T>
     {
-        private LinkedList<T> _list;
+        private LinkedList<T> _list = new LinkedList<T>();
 
-        public IEnumerator<T> GetEnumerator()
+        /// <summary>
+        /// Gets the number of items currently in the stack.
+        /// </summary>
+        public int Count
         {
-            throw new System.NotImplementedException();
+            get
+            {
+                return _list.Count;
+            }
         }
 
+        /// <summary>
+        /// Gets a value indicating whether the stack is empty.
+        /// </summary>
+        public bool IsEmpty
+        {
+            get
+            {
+                return _list.Count == 0;
+            }
+        }
+
+        /// <summary>
+        /// Adds the item to the top of the stack.
+        /// </summary>
+        /// <param name="value">The item to add.</param>
+        public void Push(T value)
+        {
+            _list.AddFirst(value);
+        }
+
+        /// <summary>
+        /// Removes and returns the top item from the stack.
+        /// </summary>
+        /// <returns>The top item from the stack.</returns>
+        public T Pop()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("The stack is empty.");
+            }
+
+            var top = _list.First.Value;
+            _list.RemoveFirst();
+
+            return top;
+        }
+
+        /// <summary>
+        /// Returns the top item from the stack without removing it.
+        /// </summary>
+        /// <returns>The top item from the stack.</returns>
+        public T Peek()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("The stack is empty.");
+            }
+
+            return _list.First.Value;
+        }
+
+        /// <summary>
+        /// Removes all items from the stack.
+        /// </summary>
+        public void Clear()
+        {
+            _list.Clear();
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the stack.
+        /// </summary>
+        /// <returns>An enumerator that can be used to iterate through the stack.</returns>
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the stack.
+        /// </summary>
+        /// <returns>An enumerator that can be used to iterate through the stack.</returns>
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new System.NotImplementedException();
+            return _list.GetEnumerator();
         }
     }
 }
