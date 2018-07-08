9e9ddb06edf7bc678ac9f92604e8c11d01e9372a
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-07-08 16:55:11 +0300

    Add Stack

---
+++ AlgorithmsAndDataStructures/Stack.List/Stack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Stack.List
+{
+    class Stack<T> : IEnumerable<T>
+    {
+        private LinkedList<T> _list;
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
