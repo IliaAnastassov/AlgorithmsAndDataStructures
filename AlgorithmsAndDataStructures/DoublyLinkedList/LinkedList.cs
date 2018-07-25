8457c9a9591ede72930f799218f0608529c9ba1d
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-07-25 16:55:22 +0300

    Update naming

--- AlgorithmsAndDataStructures/DoublyLinkedList/LinkedList.cs
+++ AlgorithmsAndDataStructures/DoublyLinkedList/LinkedList.cs
@@ -12,12 +12,12 @@ namespace DoublyLinkedList
         /// <summary>
         /// The first node in the linked list or null if empty.
         /// </summary>
-        public LinkedListNode<T> Head { get; private set; }
+        public LinkedListNode<T> First { get; private set; }
 
         /// <summary>
         /// The last node in the linked list or null if empty.
         /// </summary>
-        public LinkedListNode<T> Tail { get; private set; }
+        public LinkedListNode<T> Last { get; private set; }
 
         /// <summary>
         /// The number of items currently in the linked list.
@@ -61,17 +61,17 @@ namespace DoublyLinkedList
         /// <param name="node">The node to add.</param>
         public void AddFirst(LinkedListNode<T> node)
         {
-            var temp = Head;
-            Head = node;
-            Head.Next = temp;
+            var temp = First;
+            First = node;
+            First.Next = temp;
 
             if (IsEmpty)
             {
-                Tail = Head;
+                Last = First;
             }
             else
             {
-                temp.Previous = Head;
+                temp.Previous = First;
             }
 
             Count++;
@@ -99,15 +99,15 @@ namespace DoublyLinkedList
 
             if (IsEmpty)
             {
-                Head = node;
+                First = node;
             }
             else
             {
-                Tail.Next = node;
-                node.Previous = Tail;
+                Last.Next = node;
+                node.Previous = Last;
             }
 
-            Tail = node;
+            Last = node;
             Count++;
         }
 
@@ -118,16 +118,16 @@ namespace DoublyLinkedList
         {
             if (!IsEmpty)
             {
-                Head = Head.Next;
+                First = First.Next;
                 Count--;
 
                 if (IsEmpty)
                 {
-                    Tail = null;
+                    Last = null;
                 }
                 else
                 {
-                    Head.Previous = null;
+                    First.Previous = null;
                 }
             }
         }
@@ -141,13 +141,13 @@ namespace DoublyLinkedList
             {
                 if (Count == 1)
                 {
-                    Head = null;
-                    Tail = null;
+                    First = null;
+                    Last = null;
                 }
                 else
                 {
-                    Tail.Previous.Next = null;
-                    Tail = Tail.Previous;
+                    Last.Previous.Next = null;
+                    Last = Last.Previous;
                 }
 
                 Count--;
@@ -170,7 +170,7 @@ namespace DoublyLinkedList
         /// <returns>true if item is found in the linked list otherwise, false.</returns>
         public bool Contains(T item)
         {
-            LinkedListNode<T> current = Head;
+            LinkedListNode<T> current = First;
             while (current != null)
             {
                 if (current.Value.Equals(item))
@@ -192,7 +192,7 @@ namespace DoublyLinkedList
         /// <param name="arrayIndex">The zero-based index in array at which copying begins.</param>
         public void CopyTo(T[] array, int arrayIndex)
         {
-            var current = Head;
+            var current = First;
             while (current != null)
             {
                 array[arrayIndex++] = current.Value;
@@ -209,7 +209,7 @@ namespace DoublyLinkedList
         public bool Remove(T item)
         {
             LinkedListNode<T> previous = null;
-            LinkedListNode<T> current = Head;
+            LinkedListNode<T> current = First;
 
             while (current != null)
             {
@@ -225,7 +225,7 @@ namespace DoublyLinkedList
 
                         if (current.Next == null)
                         {
-                            Tail = previous;
+                            Last = previous;
                         }
                         else
                         {
@@ -251,7 +251,7 @@ namespace DoublyLinkedList
         /// <returns>An enumerator that can be used to iterate through the linked list.</returns>
         IEnumerator<T> IEnumerable<T>.GetEnumerator()
         {
-            var current = Head;
+            var current = First;
             while (current.Next != null)
             {
                 yield return current.Value;
@@ -273,8 +273,8 @@ namespace DoublyLinkedList
         /// </summary>
         public void Clear()
         {
-            Head = null;
-            Tail = null;
+            First = null;
+            Last = null;
             Count = 0;
         }
     }
