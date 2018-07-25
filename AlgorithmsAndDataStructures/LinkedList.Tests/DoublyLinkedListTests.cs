fca0c5d20aac0a4bb4c343dc5c243fcf13d345d3
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-07-25 17:50:44 +0300

    Add tests

---
+++ AlgorithmsAndDataStructures/LinkedList.Tests/DoublyLinkedListTests.cs
@@ -0,0 +1,84 @@
+﻿using DoublyLinkedList;
+using NUnit.Framework;
+
+
+namespace LinkedList.Tests
+{
+    [TestFixture]
+    class DoublyLinkedListTests
+    {
+        [Test]
+        public void ShouldAddFirst()
+        {
+            var sut = new LinkedList<int>();
+
+            sut.AddFirst(66);
+            sut.AddLast(42);
+            sut.AddLast(47);
+
+            Assert.That(sut.First.Value, Is.EqualTo(66));
+        }
+
+        [Test]
+        public void ShouldAddLast()
+        {
+            var sut = new LinkedList<int>();
+
+            sut.AddFirst(66);
+            sut.AddLast(42);
+            sut.AddLast(47);
+
+            Assert.That(sut.Last.Value, Is.EqualTo(47));
+        }
+
+        [Test]
+        public void ShouldRemoveFirst()
+        {
+            var sut = new LinkedList<int>
+            {
+                3, 2, 1
+            };
+
+            sut.RemoveFirst();
+
+            Assert.That(sut.First.Value, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void ShouldRemoveLast()
+        {
+            var sut = new LinkedList<int>
+            {
+                3, 2, 1
+            };
+
+            sut.RemoveLast();
+
+            Assert.That(sut.Last.Value, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void ShouldGetCount()
+        {
+            var sut = new LinkedList<int>
+            {
+                3, 2, 1
+            };
+
+            Assert.That(sut.Count, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void ShouldClear()
+        {
+            var sut = new LinkedList<int>
+            {
+                3, 2, 1
+            };
+
+            sut.Clear();
+
+            Assert.That(sut.Count, Is.EqualTo(0));
+        }
+    }
+}
