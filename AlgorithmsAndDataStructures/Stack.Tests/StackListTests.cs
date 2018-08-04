a6278bc5b397501dcfc3c0fde824fb03c097a0b4
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-08-04 23:32:52 +0300

    Add Stack test project

---
+++ AlgorithmsAndDataStructures/Stack.Tests/StackListTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Stack.List;
+
+namespace Stack.Tests
+{
+    [TestFixture]
+    public class StackListTests
+    {
+        private Stack<int> sut;
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            sut = new Stack<int>();
+        }
+
+        [Test]
+        public void ShouldPush()
+        {
+
+        }
+    }
+}
